98bf3a376d3a39f3b8c0b96555d4449c1c4c57d1
Tian Huang <ian.h.0628@gmail.com>
2020-07-14 23:14:16 +0800

    Update welcome.py

--- src/python/orquestra/welcome.py
+++ src/python/orquestra/welcome.py
@@ -1,7 +1,6 @@
 """
 This module saves a welcome message.
 """
-
 import json
 
 def welcome():
@@ -13,3 +12,15 @@ def welcome():
 
     with open("welcome.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
+        
+def zquantumtest():
+
+    from zquantum.core.circuit import (build_ansatz_circuit,
+                                       load_circuit_template,
+                                       load_circuit_template_params,
+                                       save_circuit)
+
+    ansatz = load_circuit_template('ansatz.json');
+    params = load_circuit_template_params('params.json');
+    circuit = build_ansatz_circuit(ansatz, params);
+    save_circuit(circuit, 'circuit.json')
